98ad617718b94cf4ada9afee443500cb619def13
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-05-19 11:07:21 -0400

    Inicio dos Estudos da linguagem java

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,19 @@
+
+public class TestaCaracteres {
+	
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 66;
+		System.out.println(valor);
+		
+		valor = (char) (valor + 189);
+		System.out.println(valor);
+		
+		String frase = "Curso de java ";
+		System.out.println(frase);
+	}
+	
+
+}
