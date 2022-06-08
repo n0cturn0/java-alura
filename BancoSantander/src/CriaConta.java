87b5a5058fcd4dd009065c2b1a2fa70f206a01e3
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-06-08 16:19:25 -0400

    Testando chamada de métodos

---
+++ BancoSantander/src/CriaConta.java
@@ -0,0 +1,18 @@
+
+public class CriaConta {
+	public static void main(String[] args) {
+			Conta primeiraConta = new Conta();
+			primeiraConta.saldo = 200;
+			
+			System.out.println(primeiraConta.saldo);
+			primeiraConta.saldo += 100;
+			System.out.println(primeiraConta.saldo);
+			
+			
+			
+			Conta segundaConta = new Conta();
+			segundaConta.saldo = 1000;
+			
+			System.out.println(segundaConta.saldo);
+	}
+}
---
+++ BancoSantander/src/TestaMetodo.java
@@ -0,0 +1,11 @@
+
+public class TestaMetodo {
+public static void main(String[] args) {
+	Conta MinhaConta = new Conta();
+	MinhaConta.saldo = 100;
+	MinhaConta.deposita(100);
+	System.out.println(MinhaConta.saldo);
+	
+	
+}
+}
