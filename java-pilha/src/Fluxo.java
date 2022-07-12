6b977cb48b20c90a56103add815af4714ce9f242
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-07-12 10:45:29 -0400

    try catch

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -2,13 +2,23 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
+        try {
         metodo1();
+        }  catch (ArithmeticException e) {
+			// TODO: handle exception
+        	System.out.println("ArithmeticException");
+		}
         System.out.println("Fim do main");
     }
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
+        try {
         metodo2();
+        }  catch (ArithmeticException e) {
+			// TODO: handle exception
+        	System.out.println("ArithmeticException");
+		}
         System.out.println("Fim do metodo1");
     }
 
@@ -16,6 +26,9 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+            
+            	int a = i / 0;
+            
         }
         System.out.println("Fim do metodo2");
     }
