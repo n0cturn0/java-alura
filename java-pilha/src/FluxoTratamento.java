3c77dfd934882289f77a78f598937cf71739a3f1
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-07-13 15:58:44 -0400

    Lançando exceções

--- java-pilha/src/FluxoTratamento.java
+++ java-pilha/src/FluxoTratamento.java
@@ -1,4 +1,4 @@
-public class Fluxo {
+public class FluxoTratamento {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
@@ -25,7 +25,12 @@ public class Fluxo {
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-        
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            
+            	int a = i / 0;
+            
+        }
         System.out.println("Fim do metodo2");
     }
 }
