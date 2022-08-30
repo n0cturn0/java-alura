4fd8dd60a51f45c8eba518bca135bc95ecd403a7
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-08-29 21:23:07 -0400

    Exceptions continuação

--- bytebank-herdado-conta/src/Conta.java
+++ bytebank-herdado-conta/src/Conta.java
@@ -19,22 +19,16 @@ public abstract class Conta {
 
     public abstract void deposita(double valor);
 
-    public boolean saca(double valor) {
-        if(this.saldo >= valor) {
-            this.saldo -= valor;
-            return true;
-        } else {
-            return false;
-        }
+    public void saca(double valor) {
+        if(this.saldo < valor) {
+        	throw new SaldoInsuficienteException("Saldo: " + this.saldo + ", Valor"+valor);
+         }
+        this.saldo -= valor;
     }
 
-    public boolean transfere(double valor, Conta destino) {
-        if(this.saca(valor)) {
-        		destino.deposita(valor);
-        		return true;
-        } else {
-        		return false;
-        }
+    public void transfere(double valor, Conta destino) {
+        this.saca(valor);
+        destino.deposita(valor);      		  
     }
 
     public double getSaldo(){
--- bytebank-herdado-conta/src/SaldoInsuficienteException.java
+++ bytebank-herdado-conta/src/SaldoInsuficienteException.java
@@ -3,11 +3,8 @@ public class SaldoInsuficienteException extends RuntimeException{
 
 	public SaldoInsuficienteException (String msg)
 	{
-		
+		super(msg);
 	}
 	
-	public NovoSaldoInsuficienteException (String msg)
-	{
-		//Teste
-	}
+	
 }
