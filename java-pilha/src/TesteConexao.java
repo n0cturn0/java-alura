613e900d965016624e202e7533d6f3ed8140f85e
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-09-14 15:06:13 -0400

    Finally

---
+++ java-pilha/src/TesteConexao.java
@@ -0,0 +1,20 @@
+
+public class TesteConexao {
+
+	
+	
+	public static void main(String[] args)
+	{
+		Conexao con = null;
+		try {
+			 con = new Conexao();
+			con.leDados();
+			
+		} catch (IllegalStateException ex) {
+			System.out.println("Deu erro na conexão");
+			
+		} finally {
+			con.fecha();
+		}
+	}
+}
